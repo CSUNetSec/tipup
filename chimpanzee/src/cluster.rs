b791bfe02276db2ec9f9cd6dd31359fbec6b2458
Dan Rammer <hamersaw@bushpath.com>
2017-04-10 22:10:06 -0600

    converting chimpanzee over to new proddle infrastructure.

--- chimpanzee/src/cluster.rs
+++ chimpanzee/src/cluster.rs
@@ -1,6 +1,4 @@
 use dbscan::{DBSCAN, SymmetricMatrix};
-use compute_flag_distance;
-use Flag;
 
 pub fn dbscan(flags: &Vec<Flag>, eps: f64, min_points: usize) -> (Vec<Option<usize>>, SymmetricMatrix<f64>) {
     //process results (3273 results)
@@ -14,4 +12,26 @@ pub fn dbscan(flags: &Vec<Flag>, eps: f64, min_points: usize) -> (Vec<Option<usi
 
     //complete algorithm
     (dbscan.perform_clustering(&matrix).to_owned(), matrix)
+}
+
+fn compute_flag_distance(flag_one: &Flag, flag_two: &Flag) -> f64 {
+    //domain
+    if !flag_one.domain.eq(&flag_two.domain) {
+        return 100.0;
+    }
+
+    //timestamp
+    let timestamp_difference = (flag_one.timestamp - flag_two.timestamp).abs() as f64 / 3600.0; //difference in hours
+    let timestamp_score = match timestamp_difference {
+        0.0 ... 1.0 => (timestamp_difference + 1.00027).log2() / (24.0 as f64).log2(), //logarithmic ratio
+        _ => 1.0,
+    };
+
+    //urls - TODO fuzzy match
+    /*let url_score = match flag_one.url.eq(&flag_two.url) {
+        true => 0.0,
+        false => 1.0,
+    };*/
+
+    return timestamp_score * 100.0;
 }
--- chimpanzee/src/main.rs
+++ chimpanzee/src/main.rs
@@ -1,6 +1,7 @@
-extern crate dbscan;
 #[macro_use(bson, doc)]
 extern crate bson;
+extern crate dbscan;
+extern crate docopt;
 extern crate mongodb;
 extern crate rustc_serialize;
 extern crate serde;
@@ -8,125 +9,162 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+use bson::Bson;
 use bson::oid::ObjectId;
+use docopt::Docopt;
+use mongodb::{Client, ClientOptions, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+use serde_json::Value;
+use serde_json::value::ToJson;
 
-mod cluster;
-mod dump_results;
+//mod cluster;
 
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug, Deserialize, Serialize)]
-pub enum FlagStatus {
-    Unreachable,
-    Warning,
-    Internal,
-}
+const USAGE: &'static str = "
+chimpanzee
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Flag {
-    #[serde(rename = "_id")]
-    pub id: ObjectId,
-    pub measurement_id: ObjectId,
-    pub timestamp: i64,
-    pub hostname: String,
-    pub ip_address: String,
-    pub domain: String,
-    pub domain_ip_address: Option<String>,
-    pub url: String,
-    pub status: FlagStatus,
-    pub analyzer: String,
-}
+Usage:
+    chimpanzee cluster <filename>
+    chimpanzee dump [--ca=<ca_file>] [--cert=<cert_file>] [--key=<key_file>] <mongodb_ip> [--mongodb_port=<mongodb_port>] <username> <password> <min_ts> <max_ts>
+    chimpanzee (-h | --help)
+    chimpanzee --version
 
-fn compute_flag_distance(flag_one: &Flag, flag_two: &Flag) -> f64 {
-    //domain
-    if !flag_one.domain.eq(&flag_two.domain) {
-        return 100.0;
-    }
+Options:
+    -h --help       Show this screen.
+    --version       Show version.
+    --ca            Ca ssl certification.
+    --cert          Ssl certification file.
+    --key           Ssl key file.
+    --mongodb-port  Mongodb port [default: 27017].
+";
 
-    //timestamp
-    let timestamp_difference = (flag_one.timestamp - flag_two.timestamp).abs() as f64 / 3600.0; //difference in hours
-    let timestamp_score = match timestamp_difference {
-        0.0 ... 1.0 => (timestamp_difference + 1.00027).log2() / (24.0 as f64).log2(), //logarithmic ratio
-        _ => 1.0,
-    };
-
-    //urls - TODO fuzzy match
-    /*let url_score = match flag_one.url.eq(&flag_two.url) {
-        true => 0.0,
-        false => 1.0,
-    };*/
-
-    return timestamp_score * 100.0;
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_filename: Option<String>,
+    arg_password: Option<String>,
+    arg_max_ts: Option<u64>,
+    arg_min_ts: Option<u64>,
+    arg_mongodb_ip: Option<String>,
+    arg_username: Option<String>,
+    cmd_cluster: bool,
+    cmd_dump: bool,
+    flag_ca_file: Option<String>,
+    flag_cert_file: Option<String>,
+    flag_key_file: Option<String>,
+    flag_mongodb_port: u16,
 }
 
 fn main() {
-    //dump flags from mongodb
-    //dump_results::dump_results(1488672000, 1489968000); //2017.03.05 - 2017.03.20
+    let args: Args = Docopt::new(USAGE)
+            .and_then(|d| d.decode())
+            .unwrap_or_else(|e| e.exit());
 
-    //perform dbscan
-    let flags = read_data("2017.03.05-2017.03.20.csv");
-    let (clustering, matrix) = cluster::dbscan(&flags, 65.0, 3);
+    if args.cmd_cluster {
+        /*//read flags into flag vec
+        let mut measurements = Vec::new();
+        {
+            let file = match File::open(filename) {
+                Ok(file) => file,
+                Err(e) => panic!("{}", e),
+            };
 
-    //print graphml
-    println!("<graphml>");
-    println!("\t<key id=\"d0\" for=\"node\" attr.name=\"Modularity Class\" attr.type=\"integer\"/>");
-    println!("\t<key id=\"d1\" for=\"edge\" attr.name=\"Weight\" attr.type=\"double\"/>");
-    println!("\t<graph id=\"F\" edgedefault=\"undirected\">");
-    for i in 0..flags.len() {
-        println!("\t\t<node id=\"{}\">", flags[i].id);
-        match clustering[i] {
-            Some(cluster) => println!("\t\t\t<data key=\"d0\">{}</data>", cluster),
-            None => println!("\t\t\t<data key=\"d0\">-1</data>"),
-        }
-        println!("\t\t</node>");
-    }
+            let mut buf_reader = BufReader::new(args.arg_filename.unwrap());
+            let mut line = String::new();
+            loop {
+                if let Err(e) = buf_reader.read_line(&mut line) {
+                    panic!("{}", e);
+                }
 
-    for i in 0..matrix.size() {
-        for j in i+1..matrix.size() {
-            let distance = matrix.get(i, j);
-            if distance == 100.0 {
-                continue;
+                if line.len() == 0 {
+                    break;
+                }
+
+                match serde_json::from_str::<Value>(&line) {
+                    Ok(json) => measurements.push(json),
+                    Err(e) => panic!("{}", e);
+                }
+
+                line.clear();
             }
-
-            println!("\t\t<edge source=\"{}\" target=\"{}\">", flags[i].id, flags[j].id);
-
-            let weight = (-9.0 / 100.0) * distance + 10.0;
-            println!("\t\t\t<data key=\"d1\">{}</data>", weight);
-            println!("\t\t</edge>");
         }
-    }
-    println!("\t</graph>");
-    println!("</graphml>");
-}
+ 
+        //perform dbscan
+        let (clustering, matrix) = cluster::dbscan(&flags, 65.0, 3);
 
-fn read_data(filename: &str) -> Vec<Flag> {
-    //read flags into flag vec
-    let file = match File::open(filename) {
-        Ok(file) => file,
-        Err(e) => panic!("{}", e),
-    };
-
-    let mut buf_reader = BufReader::new(file);
-    let mut line = String::new();
-    let mut flags = Vec::new();
-    loop {
-        if let Err(e) = buf_reader.read_line(&mut line) {
-            panic!("{}", e);
+        //print graphml
+        println!("<graphml>");
+        println!("\t<key id=\"d0\" for=\"node\" attr.name=\"Modularity Class\" attr.type=\"integer\"/>");
+        println!("\t<key id=\"d1\" for=\"edge\" attr.name=\"Weight\" attr.type=\"double\"/>");
+        println!("\t<graph id=\"F\" edgedefault=\"undirected\">");
+        for i in 0..flags.len() {
+            println!("\t\t<node id=\"{}\">", flags[i].id);
+            match clustering[i] {
+                Some(cluster) => println!("\t\t\t<data key=\"d0\">{}</data>", cluster),
+                None => println!("\t\t\t<data key=\"d0\">-1</data>"),
+            }
+            println!("\t\t</node>");
         }
 
-        if line.len() == 0 {
-            break;
-        }
+        for i in 0..matrix.size() {
+            for j in i+1..matrix.size() {
+                let distance = matrix.get(i, j);
+                if distance == 100.0 {
+                    continue;
+                }
 
-        let flag: Flag = match serde_json::from_str(&line) {
-            Ok(value) => value,
+                println!("\t\t<edge source=\"{}\" target=\"{}\">", flags[i].id, flags[j].id);
+
+                let weight = (-9.0 / 100.0) * distance + 10.0;
+                println!("\t\t\t<data key=\"d1\">{}</data>", weight);
+                println!("\t\t</edge>");
+            }
+        }
+        println!("\t</graph>");
+        println!("</graphml>");*/
+    } else if args.cmd_dump {
+        //connect to tipup db
+        let client = if args.flag_ca_file.is_some() && args.flag_cert_file.is_some() && args.flag_key_file.is_some() {
+            let client_options = ClientOptions::with_ssl(&args.flag_ca_file.unwrap(), 
+                    &args.flag_cert_file.unwrap(), &args.flag_key_file.unwrap(), true);
+            Client::connect_with_options(&args.arg_mongodb_ip.unwrap(), args.flag_mongodb_port, client_options)
+        } else {
+            Client::connect(&args.arg_mongodb_ip.unwrap(), args.flag_mongodb_port)
+        };
+
+        let db = match client {
+            Ok(client) => {
+                let db = client.db("proddle");
+                if let Err(e) = db.auth(&args.arg_username.unwrap(), &args.arg_password.unwrap()) {
+                    panic!("{}", e);
+                }
+                db
+            },
+            Err(e) => panic!("{}", e),
+        };
+     
+        //query proddle db for recent flags
+        let (min_ts, max_ts) = (args.arg_min_ts.unwrap(), args.arg_max_ts.unwrap());
+        let timestamp_gte_document = doc!("$gte" => min_ts);
+        let timestamp_lte_document = doc!("$lt" => max_ts);
+        let search_document = Some(doc!("timestamp" => timestamp_gte_document, "timestamp" => timestamp_lte_document));
+
+        //iterate over results
+        let cursor = match db.collection("measurements").find(search_document, None) {
+            Ok(cursor) => cursor,
             Err(e) => panic!("{}", e),
         };
 
-        flags.push(flag);
-        line.clear();
-    }
+        for document in cursor {
+            let document = match document {
+                Ok(document) => document,
+                Err(e) => panic!("{}", e),
+            };
+            
+            //TODO check if measurement is a failure
 
-    flags
+            //TODO print document
+        }
+    }
 }
